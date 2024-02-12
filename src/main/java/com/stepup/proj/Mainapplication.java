079a901288c778e8903f76c7075b29cc170b41d4
Denis Yudin <git.user1991@gmail.com>
2024-02-12 18:56:27 +0400

    Изменена логика Добавлен класс коробки

--- src/main/java/com/stepup/proj/Mainapplication.java
+++ src/main/java/com/stepup/proj/Mainapplication.java
@@ -3,6 +3,9 @@ package com.stepup.proj;
 public class Mainapplication {
     public static void main(String[] args) {
         System.out.println("Hello Russia!");
-        System.out.println("123456");
+        System.out.println("123456789");
+
+        System.out.println("234567");
+        System.out.println("345678");
     }
 }
