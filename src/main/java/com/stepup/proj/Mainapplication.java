cb098af85847537beba70c021ac3cffaf11c7879
Denis Yudin <git.user1991@gmail.com>
2024-02-12 19:14:34 +0400

    изменил логику логики

--- src/main/java/com/stepup/proj/Mainapplication.java
+++ src/main/java/com/stepup/proj/Mainapplication.java
@@ -4,10 +4,11 @@ public class Mainapplication {
     public static void main(String[] args) {
         System.out.println("Hello Russia!");
         System.out.println("123456789");
+        System.out.println("-----");
 
         System.out.println("234567");
         System.out.println("345678");
         System.out.println("11111");
-        System.out.println("22222");
+        System.out.println("22000");
     }
    }
