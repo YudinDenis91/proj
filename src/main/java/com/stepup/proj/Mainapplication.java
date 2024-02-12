f13789acfc62f6311a0ed5cfc69fc6d2b51b473d
Denis Yudin <git.user1991@gmail.com>
2024-02-12 19:10:40 +0400

    Исправления логики

--- src/main/java/com/stepup/proj/Mainapplication.java
+++ src/main/java/com/stepup/proj/Mainapplication.java
@@ -3,11 +3,11 @@ package com.stepup.proj;
 public class Mainapplication {
     public static void main(String[] args) {
         System.out.println("Hello Russia!");
-        System.out.println("123456789");
+        System.out.println("1234----789");
 
         System.out.println("234567");
         System.out.println("345678");
-        System.out.println("11111");
-        System.out.println("22222");
+        System.out.println("13331");
+        System.out.println("84597");
     }
    }
