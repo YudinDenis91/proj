3394b51a14d5598d0c5f27c119ce9757e9e9aab5
Denis Yudin <git.user1991@gmail.com>
2024-02-12 19:04:32 +0400

    Изменен метод

--- src/main/java/com/stepup/proj/Mainapplication.java
+++ src/main/java/com/stepup/proj/Mainapplication.java
@@ -7,5 +7,7 @@ public class Mainapplication {
 
         System.out.println("234567");
         System.out.println("345678");
+        System.out.println("11111");
+        System.out.println("22222");
     }
-}
+   }
