cfbcdd4ce1cbd1deffec6e6ab73ffeb2047a7235
Denis Yudin <git.user1991@gmail.com>
2024-02-12 18:45:18 +0400

    Доработка логики проекта

--- src/main/java/com/stepup/proj/Mainapplication.java
+++ src/main/java/com/stepup/proj/Mainapplication.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class Mainapplication {
     public static void main(String[] args) {
         System.out.println("Hello Russia!");
+        System.out.println("123456");
     }
 }
